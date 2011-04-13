19d708fedc6cc87af45b8614380c739c2aeec28f
Jan Ehrhardt <jan.ehrhardt@googlemail.com>
2011-04-12 21:40:40 -0700

    GPL header added

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+/**
+ * bigpipe-node is a sample on implementing bigpipe on node.js
+ * Copyright (C) 2011  Jan Ehrhardt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+
 var http = require('http');
 var jade = require('jade');
 var Task = require('parallel').Task;
